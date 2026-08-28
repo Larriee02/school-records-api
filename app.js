758601947c257372f8405c2b320fbe9c8605887d
Osondu Gabriel <136438699+OsonduGabriel@users.noreply.github.com>
2026-08-28 04:52:20 +0100

    added railway port

--- app.js
+++ app.js
@@ -54,7 +54,7 @@ async function startServer() {
       console.log("Models synchronized.");
     }
 
-    app.listen(PORT, () => {
+    app.listen(PORT, "0.0.0.0", () => {
       console.log(`Server running on ${PORT}`);
     });
   } catch (error) {
